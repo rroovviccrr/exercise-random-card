b99eb2666bc9901855c4205e633d4a72dafe2f4f
_rovi.cr <rovicr@live.com>
2021-03-16 15:53:53 +0000

    card generator

---
+++ index.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+           document.querySelector('card').classList.add(generateRandomSuit());
+           document.querySelector('card').innerHTML.generateRandomNumber();
+   };
+
+let generateRandomNumber = () => {
+    let numbers = ["A","2","3","4","5","6", "7", "8", "9", "10", "J", "Q", "K"];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+
+};
+
+let generateRandomSuit = () => {
+    let suit = ["diamond", "spade", "heart", "club"];
+    let indexSuits = Math.floor(Math.random() * suit.length);
+    return suit[indexSuits];
+};
